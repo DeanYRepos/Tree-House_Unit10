eda25e628ab9d87db1225ae6306b9c2c6ab77d5f
Dean Y <54600923+DeanYRepos@users.noreply.github.com>
2021-06-18 23:16:21 -0500

    Initial set up of Context file

--- client/src/Context.js
+++ client/src/Context.js
@@ -1,5 +1,38 @@
 //Set up context
 
 import React, { Component } from 'react';
+import Data from './Data';
 
-export const Context = React.createContext();
+const Context = React.createContext();
+export class Provider extends Component{
+
+    constructor(){
+        super();
+        this.data = new Data();
+    }
+    render(){
+    
+    const value = {
+        data: this.data,
+    }
+        
+    return(
+        <Context.Provider value={value}>
+        {this.props.children}
+        </Context.Provider>
+    );
+
+    export default function withContext(Component) {
+        return function ContextComponent(props) {
+            return(
+                <Context.Consumer>
+                    {context => <Component {...props} context={context} />}
+                </Context.Consumer>
+               );
+            }
+        }
+    
+    }
+}
+
+ 
--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom';
 //     }
 // }
 
-function Courses () {
+export function Courses () {
     const [courses, setCourses] = useState([]);
     useEffect(() => {
         
