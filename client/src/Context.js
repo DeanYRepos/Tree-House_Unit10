6d82ce7cbd891ccf14eb4eef565a454dd82f3579
Dean Y <54600923+DeanYRepos@users.noreply.github.com>
2021-08-31 23:03:28 -0500

    UserSignIn set up partially, use UserSignUp to finish

--- client/src/Context.js
+++ client/src/Context.js
@@ -20,13 +20,21 @@ export class Provider extends Component{
     
     const value = {
         data: this.data,
-    }
+        actions: {
+            signIn: this.signIn
+        }
+    };
         
     return(
         <Context.Provider value={value}>
         {this.props.children}
         </Context.Provider>
     );
+    }
+    signIn = async (emailAddress, password) => {
+        const user = await this.data.getUser(emailAddress, password);
+        return user;
+
     }
 }
         export const Consumer = Context.Consumer;
--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -1,30 +1,66 @@
 import React, { useState, useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import Header from './Header';
 import Form from './Form'
 import  { Context }  from '../Context';
 
 const UserSignIn = () => {
     const context = useContext(Context);
+    let history = useHistory();
     
     const [emailAddress, setEmailAddress] = useState("");
     const[password, setPassword] = useState("");
     const [errors, setErrors] = useState([]);
 
+    const change = (e) =>{
+      const value = e.target.value;
+    }
+ 
+    const submit = () => {
+      const users = {
+        emailAddress,
+        password,
+        
+      }
+      context.actions.signIn(users)
+      .then(user => {
+        if(user === null){
+          setErrors(()=> {
+            return {errors: ['Sign in was unsuccessful'] };
+          });
+        } else {
+          history.push('/authenticated')
+          console.log(`SUCCESS! ${emailAddress} is logged in!`);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        history.push('/error');
+      })
+    }
+    const cancel = () =>{
+      history.push('/');
+   }
 
 
     return(
         <div id="root">
-            <Header/>
-  <main>
-    <div className="form--centered">
-      <h2>Sign In</h2>
-        <Form/>
-        <p>Don't have a user account? Click here to <Link to="/signup">sign up</Link>!</p>
-    </div>
-  </main>
-</div>
-    )
+         <Header/>
+          <main>
+            <div className="form--centered">
+              <h2>Sign In</h2>
+              <form>
+                  <label htmlFor="emailAddress">Email Address</label>
+                  <input id="emailAddress" name="emailAddress" type="email" defaultValue />
+                  <label htmlFor="password">Password</label>
+                  <input id="password" name="password" type="password" defaultValue />
+                  <button className="button" type="submit">Sign In</button><button className="button button-secondary" onClick="event.preventDefault(); location.href='index.html';">Cancel</button>
+                </form>
+                <p>Don't have a user account? Click here to <Link to="/signup">sign up</Link>!</p>
+            </div>
+          </main>
+        </div>
+     )
 
 
 }
